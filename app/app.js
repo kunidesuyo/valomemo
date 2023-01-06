7cd20b1d54c324e011c4f64ee573f1fae7431615
kunidesuyo <81606310+kunidesuyo@users.noreply.github.com>
2023-01-06 18:35:05 +0900

    ejsを導入、bindがうまくできない

--- app/app.js
+++ app/app.js
@@ -3,8 +3,9 @@ const app = express()
 const port = 80
 
 app.get('/', (req, res) => {
-  res.send('nodemon test complete')
-})
+  //res.send('test complete')
+  res.render('hello.ejs');
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
