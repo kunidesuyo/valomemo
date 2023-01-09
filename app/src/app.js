06fec2b64a85b111d5f65ce94b2a47775ca4ba29
kunidesuyo <81606310+kunidesuyo@users.noreply.github.com>
2023-01-09 19:04:34 +0900

    cssとimageを使用可能にした

--- app/src/app.js
+++ app/src/app.js
@@ -2,14 +2,15 @@ const express = require('express')
 const app = express()
 const port = 80
 
-console.log('app-----------------')
-console.log(app)
 
 // なくても動くけど一応
 app.set('view engine', 'ejs')
 
 // viewsの参照先を変更
 app.set('views', '/usr/app/src/views')
+
+//cssとimageを使用可能にする
+app.use(express.static('/usr/app/src/public'));
 
 app.get('/', (req, res) => {
   //res.send('test complete')
