c9f06f5b0f618b72e7c81fc476a3eefb4b4a46f4
kunidesuyo <81606310+kunidesuyo@users.noreply.github.com>
2023-01-21 22:24:00 +0900

    削除後再レンダリングされるように修正

--- frontend/src/components/Delete.js
+++ frontend/src/components/Delete.js
@@ -7,15 +7,24 @@ import { useNavigate } from 'react-router-dom';
 export default function Delete(props) {
   let navigate = useNavigate();
 
-  const deleteData = () => {
-    console.log(props.id);
+  const deleteData = async () => {
+    //console.log(props.APIData);
+    //console.log(props.id);
     /*var params = new URLSearchParams();
     params.append("id", props.id);*/
-    axios.delete(`api/delete/${props.id}`)
+    await axios.delete(`api/delete/${props.id}`)
     .then((res) => {
       console.log(res);
-      navigate('/read');
-    })
+      console.log('axios complete')
+    });
+    console.log('before ' + props.detectDelete);
+    props.setDetectDelete((props.detectDelete + 1) % 2);
+    console.log('after ' + props.detectDelete);
+    //APIDataの対象データも削除
+    //APIData.filter((item) => item.id !== id)
+    //console.log(props.APIData);
+    //console.log('redirect')
+    //navigate('/read');
   }
   return (
     <button onClick={deleteData}>削除</button>
