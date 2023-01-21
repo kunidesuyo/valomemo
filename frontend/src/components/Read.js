da06239cd9a4432002cad35522513748f3c727fc
kunidesuyo <81606310+kunidesuyo@users.noreply.github.com>
2023-01-21 17:50:10 +0900

    delete機能作成。削除後再レンダリングされない問題あり

--- frontend/src/components/Read.js
+++ frontend/src/components/Read.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
+import Delete from './Delete'
 
 export default function Read() {
   const [APIData, setAPIData] = useState([]);
@@ -15,6 +16,7 @@ export default function Read() {
   return (
     <div>
       <h2>read</h2>
+      <Link to="/create">新規作成</Link>
       <ul>
         <li>
           <span>id </span>
@@ -26,6 +28,7 @@ export default function Read() {
               <span>{item.id} </span>
               <span>{item.content}</span>
               <Link to="/update" state={{id: item.id, content: item.content}}>更新</Link>
+              <Delete id={item.id} />
             </li>
           )
         })}
