653d93e271cbdf0ea8d92495ef8821eebd6ce2e9
kunidesuyo <81606310+kunidesuyo@users.noreply.github.com>
2023-01-23 12:33:52 +0900

    create機能をdbの変更に対応

---
+++ frontend/src/db_info.js
@@ -0,0 +1,24 @@
+//できれば環境変数で定義して、各コンテナの一貫性を保ちたい。
+export const db_column_name =
+[
+  "id",
+  "map",
+  "agent",
+  "skill",
+  "position_image",
+  "aim_image",
+  "landing_image",
+  "content"
+];
+
+export const init_db_data = 
+{
+  "id": "",
+  "map": "",
+  "agent": "",
+  "skill": "1",
+  "position_image": "position_image_test",
+  "aim_image": "aim_image_test",
+  "landing_image": "landing_image_test",
+  "content": ""
+};
