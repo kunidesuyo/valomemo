1b05240a85c01e8c2292c417d4c3da7c4e371347
kunidesuyo <81606310+kunidesuyo@users.noreply.github.com>
2023-01-23 12:33:52 +0900

    create機能をdbの変更に対応

--- frontend/src/components/Create.js
+++ frontend/src/components/Create.js
@@ -2,16 +2,24 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 
+import { db_column_name } from '../db_info';
+import { init_db_data } from '../db_info';
+
 export default function Create() {
-  const [content, setContent] = useState([]);
+  const [content, setContent] = useState(init_db_data);
   let navigate = useNavigate();
+  //console.log(db_column_name);
 
   //デバック
   useEffect(() => {console.log(content)}, [content]);
 
   const postData = () => {
     var params = new URLSearchParams();
-    params.append("content", content);
+    Object.entries(content).map(([key, value]) => {
+      if(key !== "id") {
+        params.append([key], value);
+      }
+    })
     axios.post('api/create', params)
     .then((res) => {
       //readにリダイレクト
@@ -20,14 +28,27 @@ export default function Create() {
     })
   }
 
+  const handleChange = (e) => {
+    setContent({...content, [e.target.id]: e.target.value});
+    //console.log(content);
+  }
+
   return (
-    <div>
+    <>
       <h2>create</h2>
-      <form>
-        <input onChange={(e) => {setContent(e.target.value)}}/>
-        <input onClick={postData} type="button" value="作成" />
-      </form>
-    </div>
+      {Object.entries(init_db_data).map(([key, value]) => {
+        if(key !== "id") {
+          return (
+            <>
+              <span>{key}</span>
+              <input id={key} onChange={(e) => handleChange(e)} defaultValue={value}/>
+              <br></br>
+            </>
+          )
+        }
+      })}
+      <input onClick={postData} type="button" value="作成" />
+    </>
   )
 
 }
