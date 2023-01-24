cbf65b4aa19ddf0fbe10ced319061c6f7a60d877
kunidesuyo <81606310+kunidesuyo@users.noreply.github.com>
2023-01-24 16:49:00 +0900

    read,deleteをMUIで作成

--- frontend/src/components/Read.js
+++ frontend/src/components/Read.js
@@ -3,69 +3,114 @@ import axios from 'axios';
 import { Link } from 'react-router-dom';
 import Delete from './Delete'
 
+import { db_column_name, init_db_data, agent_names, map_names, skills } from '../db_info';
+
+import Select, { SelectChangeEvent } from '@mui/material/Select';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Box from '@mui/material/Box';
+import InputLabel from '@mui/material/InputLabel';
+import Container from '@mui/material/Container';
+import Stack from '@mui/material/Stack';
+import TextField from '@mui/material/TextField';
+import Grid from '@mui/material/Grid';
+import Button from '@mui/material/Button'
+
+
 export default function Read() {
-  const [APIData, setAPIData] = useState([]);
+  const [setups, setSetups] = useState([]);
   const [detectDelete, setDetectDelete] = useState([0]);
-
-  const db_column_name =
-  [
-    "id",
-    "map",
-    "agent",
-    "skill",
-    "position_image",
-    "aim_image",
-    "landing_image",
-    "content"
-  ]
-
-  /*useEffect(() => {
-    console.log('init')
-    axios.get('/api/read')
-    .then((res) => {
-      //console.log(res.data[0]);
-      setAPIData(res.data);
-    })
-  }, []);*/
   
   useEffect(() => {
     //console.log('detect delete')
     axios.get('/api/read')
     .then((res) => {
-      //console.log(res.data[0]);
-      setAPIData(res.data);
+      setSetups(res.data);
     })
   }, [detectDelete]);
   
-  //console.log(APIData);
   return (
-    <div>
-      <h2>read</h2>
-      <Link to="/create">新規作成</Link>
-      <ul>
-        {db_column_name.map((item) => {
-          //console.log(item);
+    <>
+      <Container maxWidth="xs">
+        <Button
+          variant="contained"
+          component={Link}
+          to="/create"
+        >
+          新規作成
+        </Button>
+        {setups.map((setup) => {
           return (
-            <span>{item} </span>
+            <Box border={1} sx={{m:3}}>
+              <Grid container sx={{p:1}} spacing={1}>
+                <Grid item xs={4}>
+                  <TextField 
+                    InputProps={{readOnly: true}}
+                    value={setup.map}
+                    label="map"
+                  />
+                </Grid>
+                <Grid item xs={4}>
+                  <TextField 
+                    InputProps={{readOnly: true}}
+                    value={setup.agent}
+                    label="agent"
+                  />
+                </Grid>
+                <Grid item xs={4}>
+                  <TextField 
+                    InputProps={{readOnly: true}}
+                    value={setup.skill}
+                    label="skill"
+                  />
+                </Grid>
+              </Grid>
+
+              <Stack sx={{p:1}} spacing={1}>
+                <TextField
+                  InputProps={{readOnly: true}}
+                  value={setup.position_image}
+                  label="position_image"
+                />
+                <TextField
+                  InputProps={{readOnly: true}}
+                  value={setup.aim_image}
+                  label="aim_image"
+                />
+                <TextField
+                  InputProps={{readOnly: true}}
+                  value={setup.landing_image}
+                  label="landing_image"
+                />
+                <TextField
+                  InputProps={{readOnly: true}}
+                  value={setup.content}
+                  label="content"
+                />
+              </Stack>
+
+              {/*<Grid container sx={{p:1}} spacing={1}>
+                <Grid item xs={6}>
+                  <Button variant="contained">編集</Button>
+                </Grid>
+                <Grid item xs={6}>
+                  <Button variant="contained">削除</Button>
+                </Grid>
+              </Grid>*/}
+
+              <Button
+                variant="contained"
+                component={Link}
+                to="/update"
+                state={setup}
+              >
+                編集
+              </Button>
+              <Delete id={setup.id} detectDelete={detectDelete} setDetectDelete={setDetectDelete} />
+            </Box>
           )
         })}
-        {//コンポーネント化したい
-          APIData.map((item) => {
-          //console.log(item)
-          return (  
-            <li key={item.id}>
-              {Object.values(item).map((value) => {
-                return (
-                  <span>{value} </span>
-                )
-              })}
-              <Link to="/update" state={item}>更新</Link>
-              <Delete id={item.id} detectDelete={detectDelete} setDetectDelete={setDetectDelete} />
-            </li>
-          )
-        })}
-      </ul>
-      
-    </div>
+      </Container>
+    </>
   )
 }
