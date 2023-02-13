706c238ca8e77910cfe2a7347f700b06b6a40546
kunidesuyo <81606310+kunidesuyo@users.noreply.github.com>
2023-02-13 23:30:15 +0900

    sequelizeでdbへ接続テスト完了

--- api/src/api.js
+++ api/src/api.js
@@ -21,6 +21,24 @@ const connection = mysql.createConnection({
   database: process.env.MYSQL_DATABASE
 });
 
+const { Sequelize, DataTypes } = require('sequelize');
+
+const sequelize = new Sequelize(process.env.MYSQL_DATABASE, 'root', process.env.MYSQL_ROOT_PASSWORD, {
+  host: process.env.DB_CONTAINER_NAME,
+  dialect: 'mysql',
+  // logging: false
+});
+
+const sequelize_test = async () => {
+  try {
+    await sequelize.authenticate();
+    console.log('Connection has been established successfully.');
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+  }
+}
+sequelize_test();
+
 
 connection.connect((error) => {
   if (error) {
